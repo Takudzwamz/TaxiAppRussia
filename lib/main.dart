c2b18a3510c2928179093b0bf237fc4dc3e5b11e
Takudzwa Mupanesure <zhout1709@hotmail.com>
2021-05-04 16:26:33 +0200

    Db setiiting commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,34 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/mainpage.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final FirebaseApp app = await Firebase.initializeApp(
+    name: 'db2',
+    options: Platform.isIOS || Platform.isMacOS
+        ? const FirebaseOptions(
+            appId: '1:310804505192:ios:da389b1dc7197285cb91dd',
+            apiKey: 'AIzaSyCkec95PhJGlhmU9vZXzyesk42EVDVTuTo',
+            projectId: 'uber-clone-d6e29',
+            messagingSenderId: '310804505192',
+            databaseURL: 'https://uber-clone-d6e29-default-rtdb.europe-west1.firebasedatabase.app',
+          )
+        : const FirebaseOptions(
+            appId: '1:310804505192:android:07e57fefc9466721cb91dd',
+            apiKey: 'AIzaSyDexGaaZJSs3SrGHIUf1_9lTQcrCjbsQsQ',
+            messagingSenderId: '310804505192',
+            projectId: 'uber-clone-d6e29',
+            databaseURL: 'https://uber-clone-d6e29-default-rtdb.europe-west1.firebasedatabase.app',
+          ),
+  );
   runApp(MyApp());
 }
+
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
--- lib/screens/mainpage.dart
+++ lib/screens/mainpage.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -12,7 +13,19 @@ class _MainPageState extends State<MainPage> {
       appBar: AppBar(
         title: Text('Main Page'),
       ),
-      
+      body: Center(
+        child: MaterialButton(
+          onPressed: () {
+            DatabaseReference dbref =
+                FirebaseDatabase.instance.reference().child('Test');
+            dbref.set('isConnected');
+          },
+          height: 50,
+          minWidth: 300,
+          color: Colors.green,
+          child: Text('Test connection'),
+        ),
+      ),
     );
   }
 }
